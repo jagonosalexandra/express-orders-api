2c11a98eb0b3d57bb2761ea2c89d94036bbd3c77
unknown <jagonosalexandra@gmail.com>
2026-09-03 11:50:24 +0800

    Add /orders/:id route

--- server.js
+++ server.js
@@ -1,7 +1,40 @@
 import express from "express";
+import { readFile } from "fs/promises";
 
 const app = express();
 const PORT = 3000;
+
+app.get("/orders", async (req, res, next) => {
+  try {
+    const data = await readFile("./data.json", "utf-8");
+    const orders = JSON.parse(data);
+    res.json(orders);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/orders/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const data = await readFile("./data.json", "utf-8");
+    const orders = JSON.parse(data);
+
+    const order = orders.find((o) => o.id === id);
+
+    if (!order)
+      return res.status(404).json({ error: `Order with ID ${id} not found` });
+
+    res.json(order);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: "Something went wrong" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
