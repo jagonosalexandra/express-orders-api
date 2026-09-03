74fd1557385a63a35c778e0d07531620b9f0fdf8
unknown <jagonosalexandra@gmail.com>
2026-09-03 12:34:31 +0800

    Add 404 handler

--- server.js
+++ server.js
@@ -38,6 +38,10 @@ app.get("/orders/:id", async (req, res, next) => {
   }
 });
 
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Sorry can't find that" });
+});
+
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).json({ error: "Something went wrong" });
